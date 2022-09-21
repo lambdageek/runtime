e745b4e005fc9dd40eddd9e6f364962856f41797
Aleksey Kliger <alklig@microsoft.com>
2022-09-20 21:46:35 -0400

    WIP write up the green threads example from wasm/k

--- src/mono/sample/HelloWorld/Program.cs
+++ src/mono/sample/HelloWorld/Program.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.Collections.Generic;
 
 namespace HelloWorld
 {
@@ -11,13 +12,13 @@ namespace HelloWorld
         {
 
 	    Console.WriteLine ("before delimiting");
-	    int r = Mono.Control.Delimited.Delimit (static () => {
+	    int r = Mono.DelimitedContinuations.Delimit (static () => {
                 Console.WriteLine ("In delimited call");
 #if false
 		Console.WriteLine ("before capturing continuation");
-		int resumedAns = Mono.Control.Delimited.TransferControl<int> (static (cont) => {
+		int resumedAns = Mono.DelimitedContinuations.TransferControl<int> (static (cont) => {
 		    Console.WriteLine ("In the control handler");
-		    Mono.Control.Delimited.ResumeContinuation (cont, 42);
+		    Mono.DelimitedContinuations.ResumeContinuation (cont, 42);
 		});
 		Console.WriteLine ("After continuation resumed with value {0}", resumedAns);
 		return resumedAns + 1;
@@ -29,16 +30,74 @@ namespace HelloWorld
 	    Console.WriteLine ("After delimiter, answer {0}", r);
         }
 
-	private static Mono.Control.Delimited.ContinuationHandle<string> ConvertActionToCont(Action<string> action) {
-	    var capturedCont = Mono.Control.Delimited.TransferControl<Mono.Control.Delimited.ContinuationHandle<string>>( (retK) => {
-		string arg = Mono.Control.Delimited.TransferControl<string> ((callFKont) => {
-		    Mono.Control.Delimited.ResumeContinuation (retK, callFKont);
+	private static Mono.DelimitedContinuations.ContinuationHandle<string> ConvertActionToCont(Action<string> action) {
+	    var capturedCont = Mono.DelimitedContinuations.TransferControl<Mono.DelimitedContinuations.ContinuationHandle<string>>( (retK) => {
+		string arg = Mono.DelimitedContinuations.TransferControl<string> ((callFKont) => {
+		    Mono.DelimitedContinuations.ResumeContinuation (retK, callFKont);
 		    });
 		action (arg);
 		/* act must not return! */
 	    });
 	    return capturedCont;
 	}
+
+	private static int ThreadMain()
+	{
+	    Console.WriteLine ("A");
+	    Yield();
+	    Console.WriteLine ("B");
+	    return 1;
+	}
+
+	private static void Demo()
+	{
+	    int dummy = Mono.DelimitedContinuations.Delimit<int>(static () => {
+		ThreadCreate(ThreadMain);
+		ThreadCreate(ThreadMain);
+		JoinAllThreads();
+		return 0;
+	    });
+	}
+
+	private static Queue<Mono.DelimitedContinuations.ContinuationHandle<int>> _queue = new ();
+	private static Mono.DelimitedContinuations.ContinuationHandle<int> _after_join_K = default;
+
+	private static void ThreadCreate(Func<int> threadBody)
+	{
+	    _queue.Enqueue(Mono.DelimitedContinuations.TransferControl<Mono.DelimitedContinuations.ContinuationHandle<int>> ((enqueueK) => {
+                /* Capture the continuation before calling the thread body */
+		int dummy = Mono.DelimitedContinuations.TransferControl<int> ((beforeCallK) => {
+		    Mono.DelimitedContinuations.ResumeContinuation (enqueueK, beforeCallK);
+		});
+		int dummy2 = threadBody ();
+		/* after the thread completes either run the next suspended thread or return to the join point. */
+		if (_queue.Count == 0) {
+		    Mono.DelimitedContinuations.ResumeContinuation (_after_join_K, 123);
+		} else {
+		    Mono.DelimitedContinuations.ResumeContinuation (_queue.Dequeue(), 456);
+		}
+	    }));
+		
+	}
+
+	private static void Yield()
+	{
+	    Mono.DelimitedContinuations.TransferControl<int> ((afterYieldK) => {
+                // save the contiunation for the current thread
+		_queue.Enqueue (afterYieldK);
+		// and resume some other thread's continuation
+		Mono.DelimitedContinuations.ResumeContinuation (_queue.Dequeue (), 789);
+	    });
+	}
+
+	private static void JoinAllThreads ()
+	{
+	    Mono.DelimitedContinuations.TransferControl<int> ((afterJoinK) => {
+		_after_join_K = afterJoinK;
+		Mono.DelimitedContinuations.ResumeContinuation (_queue.Dequeue (), 999);
+	    });
+	}
+
     }
 
 }
