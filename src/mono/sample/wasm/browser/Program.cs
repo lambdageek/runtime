864d78750a329bb030bdeb1fc32ec9a389325a08
Aleksey Kliger <alklig@microsoft.com>
2022-09-22 16:04:58 -0400

    port the demo to the WASM sample

--- src/mono/sample/wasm/browser/Program.cs
+++ src/mono/sample/wasm/browser/Program.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices.JavaScript;
 using System.Runtime.InteropServices;
 
@@ -11,11 +12,70 @@ namespace Sample
     {
         public static int Main(string[] args)
         {
+            Demo();
             DisplayMeaning(42);
             return 0;
         }
 
         [JSImport("Sample.Test.displayMeaning", "main.js")]
         internal static partial void DisplayMeaning(int meaning);
+
+	private static int ThreadMain()
+	{
+	    Console.WriteLine ("A");
+	    Yield();
+	    Console.WriteLine ("B");
+	    return 1;
+	}
+
+	private static void Demo()
+	{
+	    int dummy = Mono.DelimitedContinuations.Delimit<int>(static () => {
+		ThreadCreate(ThreadMain);
+		ThreadCreate(ThreadMain);
+		JoinAllThreads();
+		return 0;
+	    });
+	}
+
+	private static Queue<Mono.DelimitedContinuations.ContinuationHandle<int>> _queue = new ();
+	private static Mono.DelimitedContinuations.ContinuationHandle<int> _after_join_K = default;
+
+	private static void ThreadCreate(Func<int> threadBody)
+	{
+	    _queue.Enqueue(Mono.DelimitedContinuations.TransferControl<Mono.DelimitedContinuations.ContinuationHandle<int>> ((enqueueK) => {
+                /* Capture the continuation before calling the thread body */
+		int dummy = Mono.DelimitedContinuations.TransferControl<int> ((beforeCallK) => {
+		    enqueueK.Resume(beforeCallK);
+		});
+		int dummy2 = threadBody ();
+		/* after the thread completes either run the next suspended thread or return to the join point. */
+		if (_queue.Count == 0) {
+		    _after_join_K.Resume(123);
+		} else {
+                    _queue.Dequeue().Resume(456);
+                }
+            }));
+        }
+
+        private static void Yield()
+        {
+            Mono.DelimitedContinuations.TransferControl<int> ((afterYieldK) => {
+                // save the contiunation for the current thread
+                _queue.Enqueue (afterYieldK);
+                // and resume some other thread's continuation
+                _queue.Dequeue().Resume(789);
+            });
+        }
+
+	private static void JoinAllThreads ()
+	{
+	    Mono.DelimitedContinuations.TransferControl<int> ((afterJoinK) => {
+		_after_join_K = afterJoinK;
+		_queue.Dequeue().Resume(999);
+	    });
+	}
+
     }
+
 }
